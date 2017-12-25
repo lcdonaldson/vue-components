c8eb3af2812cb0f7ba1151ca4237f258b7d56de7
Levi Donaldson <lcdbacanas@gmail.com>
2017-12-24 19:45:21 -0600

    style changes to components

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
-
-Vue.component('product-list', {
-    template: '<div><product v-for="product in products">{{product.product}}</product></div>',
-
-    data() {
-        return {
-            products: [
-                { product: 'Rod', instock: true },
-                { product: 'Reel', instock: true },
-                { product: 'Mono', instock: true },
-                { product: 'Braid', instock: false },
-                { product: 'Baits', instock: true }
-            ]
-        };
-    }
-});
-
 Vue.component('message', {
-    
+
     props: ['title', 'body'],
 
     template: `
-        <article class = "center mw5 mw6-ns hidden ba mv4">
+        <article class = "center mw5 mw6-ns hidden ba mt4">
             <h1 class = "f4 bg-near-black white mv0 pv2 ph3"> {{ title }} </h1>
 
             <div class = "pa3 bt">
@@ -29,6 +12,23 @@ Vue.component('message', {
         </article>     
     `
 });
+
+Vue.component('product-list', {
+    template: '<div class="b--black bg-light-gray mv3 w-100 w-50-m w-25-l mw5 pa1 center tc"><product v-for="product in products">{{product.product}}</product></div>',
+
+    data() {
+        return {
+            products: [
+                { product: 'Rods', instock: true },
+                { product: 'Reels', instock: true },
+                { product: 'Mono', instock: true },
+                { product: 'Braid', instock: false },
+                { product: 'Bait', instock: true }
+            ]
+        };
+    }
+});
+
 
 Vue.component('product', {
     template: '<li><slot></slot></li>'

4293176f45cd148320d0584f4a36713ad7a3f473
Levi Donaldson <lcdbacanas@gmail.com>
2018-09-14 00:29:54 -0500

    Update ToggleButton.vue

--- toggle-button/ToggleButton.vue
+++ toggle-button/ToggleButton.vue
@@ -1,59 +1,50 @@
 <template>
-<html>
-	<head>
-		<title>Vue.js test</title>
-		<script src="https://unpkg.com/vue"></script>
-	</head>
 	<body>
-        <div id="app" class="heroWrapper hwSkin" v-on:click="isOpen = !isOpen">
-            <p v-show="isOpen">{{nmessage}}</p>
-            <p v-show="!isOpen">{{message}}</p> 
-        </div> 
+		<div id="app" class="heroWrapper hwSkin" v-on:click="isOpen = !isOpen">
+		    <p v-show="isOpen">{{nmessage}}</p>
+		    <p v-show="!isOpen">{{message}}</p> 
+		</div> 
 	</body>
 </template>
 
+<script>
+	var app = new Vue({
+	  el: '#app',
+	  data: { 
+		message: 'Logged In',
+		nmessage: 'Logged Out', 
+		isOpen: false 
+	  }
+	});
+</script>
+
 <style>
-*{
-    margin: 0;
-    padding: 0;
-}
+	* {
+		margin: 0;
+		padding: 0;
+	}
 
+	body {
+		padding: 0.2rem; 
+		background: radial-gradient(lightblue, #0a77d5);  
+		font-family: "helvetica";
+	}
 
-body {
-	padding: 0.2rem; 
-	background: radial-gradient(lightblue, #0a77d5);  
-    font-family: "helvetica";
-}
+	.heroWrapper {
+		padding: 1rem;
+		max-width: 9rem;
+		margin: 3rem auto;
+		font-size: 1.5em; 
+	}
 
-.heroWrapper {
-    padding: 1rem;
-    max-width: 9rem;
-    margin: 3rem auto;
-    font-size: 1.5em; 
-}
+	.hwSkin {
+		background: linear-gradient(lightseagreen, skyblue);
+		color: gainsboro;
+		font-weight: 600;
+		border-radius: 1.5em; 
+	} 
 
-.hwSkin {
-    background: linear-gradient(lightseagreen, skyblue);
-    color: gainsboro;
-    font-weight: 600;
-    border-radius: 1.5em; 
-} 
-
-p { text-align: center; }
+	p { text-align: center; }
 </style>
 
-<script>
-window.onload = (function () {
-    
-    var app = new Vue({
-      el: '#app',
-      data: { 
-        message: 'Logged In',
-        nmessage: 'Logged Out', 
-        isOpen: false 
-      }
-      
-    })
-    
-})
-</script>
+

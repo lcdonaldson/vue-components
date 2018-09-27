38eeecc7b2f6ef74f52b36e46a81471ab1b255b8
Levi Donaldson <lcdbacanas@gmail.com>
2018-09-26 20:39:09 -0500

    Create VideoList.vue

---
+++ VideoList.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul>
+        <VideoListItem
+            v-for="video in videos"
+            :video="video"
+            :key="video"
+        >
+        </VideoListItem>
+    </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem';
+export default {
+    name: 'VideoList',
+    components: {
+        VideoListItem
+    },
+    props: {
+        videos: Array
+    }
+}
+</script>
+
+<script>
+</script>
